d97be05c4ea645da0a646418d51e92e42ce6bb17
Pedro Pacheco <pacheckdev@gmail.com>
2021-05-19 22:10:26 -0300

    finished

---
+++ index.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5];
+
+console.log(arr);
+
+arr.unshift('Pedro');
+
+arr.pop();
+
+arr.push('Luan');
+
+arr.push('Dandara');
+
+arr.shift();
+
+const numbers = [7,5,6,3,8,9,2,1,4];
+
+const orderedNumbers = numbers.sort();
